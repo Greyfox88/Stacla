b19d3525c6fbb64c20995c45d9f903dd58e5f2ec
Greyfox88 <greyfox_maquis@hotmail.com>
2024-07-07 19:56:09 +0100

    Filters for new presets
    Added basic modals for filtering new presets

--- src/app/components/assets/tab-assets/tab-assets.component.ts
+++ src/app/components/assets/tab-assets/tab-assets.component.ts
@@ -2,12 +2,14 @@ import { Component,inject } from '@angular/core';
 import { AssetlistComponent } from '../assetlist/assetlist.component';
 import { AssetService } from '../../../services/asset/asset.service';
 import { Asset } from '../../../services/database/database.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tab-assets',
   standalone: true,
   imports: [
-    AssetlistComponent
+    AssetlistComponent,
+    FormsModule
   ],
   templateUrl: './tab-assets.component.html',
   styleUrl: './tab-assets.component.scss'
@@ -17,6 +19,11 @@ export class TabAssetsComponent {
   eras = [...new Set(this.assetService.masterAssetList.map(item => item.Era))];
   factions = [...new Set(this.assetService.masterAssetList.map(item => item.Faction))];
   types = [...new Set(this.assetService.masterAssetList.map(item => item.Type))];
+  filteredAssetList = this.assetService.masterAssetList;
+  //Warch out, these are currently magic string used in the filter below
+  eraFilter = "Era";
+  factionFilter = "Faction";
+  typeFilter = "Type";
 
   newAsset(){
     this.assetService.newAsset();
@@ -27,5 +34,10 @@ export class TabAssetsComponent {
     asset.Id = undefined;
     asset.CampaignId = this.assetService.campaignId;
     this.assetService.putAsset(asset);
+  }
+
+  filterChange()
+  {
+    this.filteredAssetList = this.assetService.masterAssetList.filter((asset) => (this.eraFilter == "Era" || asset.Era == this.eraFilter) && (this.factionFilter == "Faction" || asset.Faction == this.factionFilter) && (this.typeFilter == "Type" || asset.Type == this.typeFilter))
   }
 }
--- src/app/components/characters/tab-characters/tab-characters.component.ts
+++ src/app/components/characters/tab-characters/tab-characters.component.ts
@@ -2,18 +2,29 @@ import { Component,inject } from '@angular/core';
 import { CharacterlistComponent } from '../characterlist/characterlist.component';
 import { CharacterService } from '../../../services/character/character.service';
 import { Character } from '../../../services/database/database.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tab-characters',
   standalone: true,
   imports: [
-    CharacterlistComponent
+    CharacterlistComponent,
+    FormsModule
   ],
   templateUrl: './tab-characters.component.html',
   styleUrl: './tab-characters.component.scss'
 })
 export class TabCharactersComponent {
   characterService = inject(CharacterService);
+  eras = [...new Set(this.characterService.masterCharacterList.map(item => item.Era))];
+  factions = [...new Set(this.characterService.masterCharacterList.map(item => item.Faction))];
+  traits = [...new Set(this.characterService.masterCharacterList.map(item => item.Traits))];
+  filteredCharacterList = this.characterService.masterCharacterList;
+  //Warch out, these are currently magic string used in the filter below
+  eraFilter = "Era";
+  factionFilter = "Faction";
+  traitFilter = "Trait";
+
 
   newCharacter(){
     this.characterService.newCharacter();
@@ -24,5 +35,10 @@ export class TabCharactersComponent {
     character.Id = undefined;
     character.CampaignId = this.characterService.campaignId;
     this.characterService.putCharacter(character);
+  }
+
+  filterChange()
+  {
+    this.filteredCharacterList = this.characterService.masterCharacterList.filter((character) => (this.eraFilter == "Era" || character.Era == this.eraFilter) && (this.factionFilter == "Faction" || character.Faction == this.factionFilter) && (this.traitFilter == "Trait" || character.Traits == this.traitFilter))
   }
 }
